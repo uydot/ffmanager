4f7b258fcb3197783217cb498d0cbc31d12e5462
rooksit <rooksit@gmail.com>
2022-03-03 23:47:34 -0300

    update

--- src/__mocks__/actividades.js
+++ src/__mocks__/actividades.js
@@ -1,54 +1,54 @@
 
 export const columns = [
-    { field: 'name', headerName: 'Material', width: 180, editable: false },
-    { field: 'cantidad', headerName: 'Cantidad', type: 'number', editable: true },
-    { field: 'stock', headerName: 'Stock', type: 'number', editable: false },
+    { field: 'name', headerName: 'Actividad', width: 180, editable: false },
+    { field: 'duracion', headerName: 'Duracion', type: 'number', editable: true },
+    { field: 'stock', headerName: 'Orden', type: 'number', editable: true },
   ];
   
   export const rows = [
     {
       id: 1,
       name: "Rondos",
-      cantidad: 0,
-      stock: 40
+      duracion: 0,
+      stock: 0
       
     },
     {
       id: 2,
       name: "Posesión",
-      cantidad: 39,
-      stock: 39
+      duracion: 39,
+      stock: 0
       
     },
     {
       id: 3,
       name: "Juego de Posición",
-      cantidad: 19,
-      stock: 19,
+      duracion: 19,
+      stock: 0,
     },
     {
       id: 4,
       name: "Ataque-Defensa",
-      cantidad: 0,
-      stock: 30,
+      duracion: 0,
+      stock: 0,
     },
     {
       id: 5,
       name: "Finalizaciones",
-      cantidad: 0,
-      stock: 23,
+      duracion: 0,
+      stock: 0,
     },
     {
         id: 6,
         name: "Partido Modificado",
-        cantidad: 0,
-        stock: 23,
+        duracion: 0,
+        stock: 0,
       },
       {
         id: 7,
         name: "Fútbol",
-        cantidad: 0,
-        stock: 23,
+        duracion: 0,
+        stock: 0,
       },
   ];
   
